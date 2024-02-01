d978f6adc2a735a3478c3f0467eed432cc850cdc
Dohun Lee <nestour95@gmail.com>
2024-02-01 15:05:26 +0900

    feat: OpenVidu 기본 세팅

---
+++ BackEnd/chatting/src/main/java/code/odyssey/domain/openvidu/controller/OpenViduController.java
@@ -0,0 +1,54 @@
+package code.odyssey.domain.openvidu.controller;
+
+import io.openvidu.java.client.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+/**
+ * 세션 아이디 (임의의 값을 지정해야함) 기반으로 영상통화 세션을 만들고 토큰을 프론트에 반환해야함
+ */
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@RestController
+public class OpenViduController {
+
+    private final OpenVidu openVidu;
+
+    /**
+     * @param params The Session properties
+     * @return The Session ID
+     */
+    @PostMapping("/api/sessions")
+    public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        Session session = openVidu.createSession(properties);
+        return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+    }
+
+    /**
+     * @param sessionId The Session in which to create the Connection
+     * @param params    The Connection properties
+     * @return The Token associated to the Connection
+     */
+    @PostMapping("/api/sessions/{sessionId}/connections")
+    public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
+                                                   @RequestBody(required = false) Map<String, Object> params)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+
+        Session session = openVidu.getActiveSession(sessionId);
+        if (session == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+        Connection connection = session.createConnection(properties);
+        return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+    }
+
+
+}
---
+++ BackEnd/chatting/src/main/java/code/odyssey/global/config/OpenViduConfig.java
@@ -0,0 +1,26 @@
+package code.odyssey.global.config;
+
+import io.openvidu.java.client.OpenVidu;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class OpenViduConfig {
+
+    @Value("${OPENVIDU_URL}")
+    private String OPENVIDU_URL;
+
+    @Value("${OPENVIDU_SECRET}")
+    private String OPENVIDU_SECRET;
+
+
+    @Bean
+    public OpenVidu openVidu() {
+        return new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
+
+
+}
